78a6c49dccdfcd22c6ba80d676248a98b08a962f
Joseph Morales <jmdevop@gmail.com>
2019-11-26 21:00:58 -0500

    updated wiretap.py - added command line options

--- wiretap.py
+++ wiretap.py
@@ -23,7 +23,7 @@ def Record():
 	hash = hash.hexdigest().upper()
 	# Filename to Save As
 	now = datetime.now()
-        dt = now.strftime("%H%M%S-%Y")
+	dt = now.strftime("%H%M%S-%Y")
 	filename = hash + "--" + dt + ".wav"
 	print ('{:*^69}'.format(" " + Colors.Header + "WireTAP" + Colors.Normal + " "))
 	try:
@@ -34,9 +34,31 @@ def Record():
 		write(filename, 44100, record)
 	except IOError:
 		print ("[" + Colors.Fail + "X" + Colors.Normal + "] Error Saving File, Check Permissions")
-	except Exception as e:
+	except Exception:
 		print ("[" + Colors.Fail + "X" + Colors.Normal + "] An Unknown Error Has Occured")
-		print (e.message, e.args)
+
+def Help():
+	print ("Usage:")
+	print ("	python wiretap.py --record			- " + Colors.Header + "Starts recording" + Colors.Normal)
+	print ("	python wiretap.py --delete [filename]		- " + Colors.Header + "Deletes .wav Recording" + Colors.Normal)
+	print ("	python wiretap.py --upload-ftp [filename]	- " + Colors.Header + "Uploads file to FTP Server (Not Implemented Yet)" + Colors.Normal)
+	print ("	python wiretap.py --help			- " + Colors.Header + "Displays this message" + Colors.Normal)
 
 if __name__ == '__main__':
-	Record()
+	if(len(sys.argv) > 1):
+		if (sys.argv[1] == "--record"):
+			Record()
+		elif (sys.argv[1] == "--delete"):
+			if (sys.argv[2].endswith('.wav')):
+				os.remove(sys.argv[2])
+				print ("Deleting:" + sys.argv[2])
+			else:
+				print ("Not a vaild .wav file")
+		elif (sys.argv[1] == "--upload-ftp"):
+			print ("This function will be added in a later release")
+		elif (sys.argv[1] == "--help"):
+			Help()
+		else:
+			Help()
+	else:
+		Help()
